a9d2e5e9e463d4022cc817dd951c6b14b6ade8ad
poojitamagesh <36636467+poojitamagesh@users.noreply.github.com>
2019-08-12 21:05:20 +0530

    Create sock-merchant.js

---
+++ warm-up/sock-merchant.js
@@ -0,0 +1,34 @@
+//
+// HackerRank Warm-Up Solutions 
+//
+// Sock Merchant
+//
+// Author - Poojita Magesh
+// https://www.hackerrank.com/challenges/sock-merchant/problem
+
+// Solution:
+
+// Solution function
+
+n = 9; //array length
+
+ar = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+
+function sockMerchant(n, ar) {
+    ar = ar.sort();
+    var pair = 0;
+    //create a map for storing frequency
+    var numMap = {};
+    //loop throught the sock colours
+    for (let num of ar) {
+        //increase the frequency by 1 or initialize with 1 if encountered for the first time
+        numMap[num] = numMap[num] + 1 || 1;
+        //if it's a pair, add 1 to pair
+        if ((numMap[num] % 2) == 0)
+            pair++;
+    }
+    return pair;
+}
+
+//output
+console.log(sockMerchant(n, ar));
