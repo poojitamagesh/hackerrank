617697a1df43bb62fb4d2c7b20a3ce306f5c5ccc
Poojita Magesh <36636467+poojitamagesh@users.noreply.github.com>
2019-08-18 22:03:56 +0530

    Create array-2d.js

---
+++ arrays/2d-array-ds/array-2d.js
@@ -0,0 +1,32 @@
+//
+// HackerRank Arrays Challenge Solutions 
+//
+// Jumping on the Clouds
+//
+// Author - Poojita Magesh
+// https://www.hackerrank.com/challenges/2d-array/problem
+
+// Solution:
+
+//input array
+arr = [[1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 0, 2, 4, 4, 0], [0, 0, 0, 2, 0, 0], [0, 0, 1, 2, 4, 0]];
+
+//solution function
+
+function hourglassSum(arr) {
+    var sum = 0;
+    //array to store sums of each hourglass
+    var temp = [];
+    for (var i = 0; i < 4; i++){
+        // i/j should be less than 4 since i/j = 3 is the highest intial i/j value to calculate an hourglass sum
+        sum = 0;
+        for (var j = 0; j < 4; j++){
+            sum = arr[i][j] + arr[i][j+1] + arr[i][j + 2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+            temp.push(sum);
+        }
+    }
+    return Math.max(...temp);
+}
+
+//output
+console.log(hourglassSum(arr));
